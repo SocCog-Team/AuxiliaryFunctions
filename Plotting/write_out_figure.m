383d1be6c0a63cfb677a467c04878d9471c45804
Sebastian Moeller <moeller0@gmx.de>
2017-06-19 17:13:53 +0200

    "Teach write_out_figure.m to deal with the new Matlab r2016a figure to print scaling."

--- Plotting/write_out_figure.m
+++ Plotting/write_out_figure.m
@@ -1,4 +1,4 @@
-function [ ret_val ] = write_out_figure(img_fh, outfile_fqn, verbosity_str)
+function [ ret_val ] = write_out_figure(img_fh, outfile_fqn, verbosity_str, print_options_str)
 %WRITE_OUT_FIGURE save the figure referenced by img_fh to outfile_fqn,
 % using .ext of outfile_fqn to decide which image type to save as.
 %   Detailed explanation goes here
@@ -8,6 +8,20 @@ if ~exist('verbosity_str', 'var')
 	verbosity_str = 'verbose';
 end
 
+% deal with r2016a changes, needs revision
+if (strcmp(version('-release'), '2016a'))
+	set(img_fh, 'PaperPositionMode', 'manual');
+	print_options_str = '-bestfit';
+end
+
+if ~exist('print_options_str', 'var') || isempty(print_options_str)
+	print_options_str = '';
+else
+	print_options_str = [', ''', print_options_str, ''''];
+end
+resolution_str = ', ''-r600''';
+
+
 
 % check whether the path exists, create if not...
 [pathstr, name, img_type] = fileparts(outfile_fqn);
@@ -15,28 +29,42 @@ if isempty(dir(pathstr)),
 	mkdir(pathstr);
 end
 
+device_str = [];
+
 switch img_type(2:end)
 	case 'pdf'
 		% pdf in 7.3.0 is slightly buggy...
-		print(img_fh, '-dpdf', outfile_fqn);
-	case 'ps'
-		print(img_fh, '-depsc2', outfile_fqn);
+		%print(img_fh, '-dpdf', outfile_fqn);
+		device_str = '-dpdf';
+	case 'ps3'
+		%print(img_fh, '-depsc2', outfile_fqn);
+		device_str = '-depsc';
+	case {'ps', 'ps2'}
+		%print(img_fh, '-depsc2', outfile_fqn);
+		device_str = '-depsc2';
 	case {'tiff', 'tif'}
 		% tiff creates a figure
-		print(img_fh, '-dtiff', outfile_fqn);
+		%print(img_fh, '-dtiff', outfile_fqn);
+		device_str = '-dtiff';
 	case 'png'
 		% tiff creates a figure
-		print(img_fh, '-dpng', outfile_fqn);
-	case 'fig'	
-		%sm: allows to save figures for further refinements
-		saveas(img_fh, outfile_fqn, 'fig');		
+		%print(img_fh, '-dpng', outfile_fqn);
+		device_str = '-dpng';
 	case 'eps'
-		print (img_fh, '-depsc', '-r300', outfile_fqn); 
+		%print(img_fh, '-depsc', '-r300', outfile_fqn);
+		device_str = '-depsc';
+	case 'fig'
+		%sm: allows to save figures for further refinements
+		saveas(img_fh, outfile_fqn, 'fig');
 	otherwise
 		% default to uncompressed images
 		disp(['Image type: ', img_type, ' not handled yet...']);
-		% write out the mosaic as image, sadly the compression does not work...
-		% 		imwrite(mos, [outfile_fqn, '.tif'], format, 'Compression', 'none');
+end
+
+if ~isempty(device_str)
+	device_str = [', ''', device_str, ''''];
+	command_str = ['print(img_fh', device_str, print_options_str, resolution_str, ', outfile_fqn)'];
+	eval(command_str);
 end
 
 if strcmp(verbosity_str, 'verbose')
@@ -46,6 +74,8 @@ if strcmp(verbosity_str, 'verbose')
 		disp(['Saved figure (', num2str(img_fh), ') to: ', outfile_fqn]);			% older Matlab has numeric figure handles
 	end
 end
+
 ret_val = 0;
+
 return
 end
