46f09b9cb68b314b3a19cff5e76158a320746faf
Sebastian Moeller <moeller0@gmx.de>
2017-10-11 12:36:49 +0200

    ""

--- Plotting/fnFormatDefaultAxes.m
+++ Plotting/fnFormatDefaultAxes.m
@@ -5,6 +5,9 @@ function [ ] = fnFormatDefaultAxes( type )
 %   type is simple a unique string to select the requested set
 % 20070827sm: changed default output formatting to allow pretty paper output
 switch type
+    
+    case 'DPZ2017Evaluation'
+        set(0, 'DefaultAxesLineWidth', 2.0, 'DefaultAxesFontName', 'Arial', 'DefaultAxesFontSize', 20, 'DefaultAxesFontWeight', 'bold');    
     case '16to9slides'
         set(0, 'DefaultAxesLineWidth', 1.5, 'DefaultAxesFontName', 'Helvetica', 'DefaultAxesFontSize', 24, 'DefaultAxesFontWeight', 'bold');
     case 'fp_paper'
