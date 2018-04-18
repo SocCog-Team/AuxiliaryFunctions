44dad2aaac40f4cf342994e97866f583482c4d9c
Sebastian Moeller <moeller0@gmx.de>
2018-04-18 10:02:26 +0200

    "Default to also open subject_bias_analysis_sm01.m in the editor and switch order to add LogFileAnalysis first followed by SessionDataAnalysis; so that the current main starting point is in the open editor tab ready to be run, after AddSCPCodeTOMatlabPath is finished. "

--- AddSCPCodeToMatlabPath.m
+++ AddSCPCodeToMatlabPath.m
@@ -22,8 +22,8 @@ SCPDirs = GetDirectoriesByHostName(override_directive);
 %AddToMatlabPath( fullfile(SCPDirs.SCP_CODE_BaseDir), [], [] );
 % selectively add the different code repositories
 AddToMatlabPath( fullfile(SCPDirs.SCP_CODE_BaseDir, 'AuxiliaryFunctions'), [], [] );
-AddToMatlabPath( fullfile(SCPDirs.SCP_CODE_BaseDir, 'SessionDataAnalysis'), [], [] );
 AddToMatlabPath( fullfile(SCPDirs.SCP_CODE_BaseDir, 'LogFileAnalysis'), [], fullfile(SCPDirs.SCP_CODE_BaseDir, 'LogFileAnalysis', 'fnParseEventIDEReportSCPv06.m') );
+AddToMatlabPath( fullfile(SCPDirs.SCP_CODE_BaseDir, 'SessionDataAnalysis'), [], fullfile(SCPDirs.SCP_CODE_BaseDir, 'SessionDataAnalysis', 'subject_bias_analysis_sm01.m') );
 AddToMatlabPath( fullfile(SCPDirs.SCP_CODE_BaseDir, 'eyetrackerDataAnalysis'), [], [] );
 AddToMatlabPath( fullfile(SCPDirs.SCP_CODE_BaseDir, 'coordination_testing'), [], [] );
 
