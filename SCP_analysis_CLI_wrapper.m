2164eb5292bf27f54f04d7ac8b0a6148141ebc69
Sebastian Moeller <moeller0@gmx.de>
2018-04-23 13:34:31 +0200

    "This script needs a local copy of fnIsMatlabRunningInTextMode, so give it one."

--- SCP_analysis_CLI_wrapper.m
+++ SCP_analysis_CLI_wrapper.m
@@ -30,3 +30,17 @@ end
 
 end
 
+function [ running_in_text_mode ] = fnIsMatlabRunningInTextMode( input_args )
+%FNISMATLABRUNNINGFROMCLI is this matlab instance running as textmode
+%application
+%   Detailed explanation goes here
+
+running_in_text_mode = 0;
+
+if (~usejava('awt'))
+    running_in_text_mode = 1;
+end
+
+return
+end
+
