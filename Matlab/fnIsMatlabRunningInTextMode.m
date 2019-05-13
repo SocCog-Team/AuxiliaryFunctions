c2a41fc2ce817ee7959a938fdee43dfd9ee945d1
Sebastian Moeller <moeller0@gmx.de>
2019-05-13 16:59:39 +0200

    "Switch to tabbed indentation."

--- Matlab/fnIsMatlabRunningInTextMode.m
+++ Matlab/fnIsMatlabRunningInTextMode.m
@@ -6,7 +6,7 @@ function [ running_in_text_mode ] = fnIsMatlabRunningInTextMode( input_args )
 running_in_text_mode = 0;
 
 if (~usejava('awt'))
-    running_in_text_mode = 1;
+	running_in_text_mode = 1;
 end
 
 return
