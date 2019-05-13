860ab4e0e9357453ffe0d910a62a418b4fd5b2d1
Sebastian Moeller <moeller0@gmx.de>
2019-05-13 16:58:38 +0200

    "Switch to tabbed indentation."

--- Matlab/fnSaveDirectoryMatFileForHost.m
+++ Matlab/fnSaveDirectoryMatFileForHost.m
@@ -1,29 +1,29 @@
 function [ output_args ] = fnSaveDirectoryMatFileForHost( local_SCP_DATA_BaseDir, local_SCP_CODE_BaseDir, remote_SCP_DATA_BaseDir, remote_SCP_CODE_BaseDir )
 %FNSAVEDIRECTORYMATFILEFORHOST Summary of this function goes here
-%   This can be used to create a cononical per-host directory definition
+%   This can be used to create a canonical per-host directory definition
 %   mat file
 
 % get the directory this mfile lives in
 [current_dir, ~, ~] = fileparts(mfilename('fullpath'));
 
 if ~exist('local_SCP_DATA_BaseDir', 'var')
-    local_SCP_DATA_BaseDir = fullfile('C:');
+	local_SCP_DATA_BaseDir = fullfile('C:');
 end
 local.SCP_DATA_BaseDir = local_SCP_DATA_BaseDir;
 
 if ~exist('local_SCP_CODE_BaseDir', 'var')
-    local_SCP_CODE_BaseDir = fullfile('C:', 'SCP_CODE');
+	local_SCP_CODE_BaseDir = fullfile('C:', 'SCP_CODE');
 end
 local.SCP_CODE_BaseDir = local_SCP_CODE_BaseDir;
 
 
 if ~exist('remote_SCP_DATA_BaseDir', 'var')
-    remote_SCP_DATA_BaseDir = fullfile('Z:', 'taskcontroller');
+	remote_SCP_DATA_BaseDir = fullfile('Z:', 'taskcontroller');
 end
 remote.SCP_DATA_BaseDir = remote_SCP_DATA_BaseDir;
 
 if ~exist('remote_SCP_CODE_BaseDir', 'var')
-    remote_SCP_CODE_BaseDir = fullfile(remote.SCP_DATA_BaseDir, 'CODE');
+	remote_SCP_CODE_BaseDir = fullfile(remote.SCP_DATA_BaseDir, 'CODE');
 end
 remote.SCP_CODE_BaseDir = remote_SCP_CODE_BaseDir;
 
@@ -36,7 +36,7 @@ CurrentShortHostName = CurrentHostName; % if the hostnmae has no domain part the
 % extract the short host name by removing the domain parts
 dot_idx=strfind(CurrentHostName, '.');
 if ~isempty(dot_idx)
-    CurrentShortHostName = CurrentHostName(1:dot_idx(1)-1);
+	CurrentShortHostName = CurrentHostName(1:dot_idx(1)-1);
 end
 
 disp(['Saving directory definitions to ', fullfile(current_dir, [CurrentShortHostName, '.mat'])]);
