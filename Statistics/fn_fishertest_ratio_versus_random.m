2633a2778f1065b55a3b3b87e7ec1e6d0f097dcd
taskcontroller <taskcontroller@PC40687.dpz.lokal>
2024-03-06 16:54:12 +0100

    Helper function to allow testing a two item ratio_count vector using a fisher test

---
+++ Statistics/fn_fishertest_ratio_versus_random.m
@@ -0,0 +1,29 @@
+function [fisher_table] = fn_fishertest_ratio_versus_random( ratio_count )
+%FN_FISHERTEST_RATIO_VERSUS_RANDOM Summary of this function goes here
+%   Detailed explanation goes here
+
+fisher_table = zeros(2);
+fisher_table(:,1) = ratio_count;
+odd_number_handling_method = 'increase_by_one'; % increase_by_one, split_unfavorably
+
+n_total = sum(ratio_count);
+switch odd_number_handling_method
+	case 'increase_by_one'
+		% if odd increase by one to test against 50% ratio
+		if mod(n_total, 2)
+			n_total = n_total + 1;
+		end
+		fisher_table(:,2) = [n_total/2, n_total/2];
+	case 'split_unfavorably'
+		% keep the number the same, but bias in the same direction as ratio
+		% is biased which will result in a conservative test.
+		if ratio(1) > ratio(2)
+			fisher_table(:,2) = [ceil(n_total/2), floor(n_total/2)];
+		else
+			fisher_table(:,2) = [floor(n_total/2), ceil(n_total/2)];
+		end
+end
+
+return
+end
+
